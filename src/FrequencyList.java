012a6d76ae27b3d0d070489fdf618067e10db8c4
Tommy Subarc <tsubaric@uiowa.edu>
2021-09-15 10:58:33 -0500

    re-HW2

--- src/FrequencyList.java
+++ src/FrequencyList.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class FrequencyList
-{
+public class FrequencyList {
     /*
       Given a single String word, traverse the String[] line
       to find the number of times word is given in line
@@ -14,11 +13,15 @@ public class FrequencyList
       output:
         int: frequency of word in the array line
      */
-    public static int get_word_frequencies(String word, String[] line)
-    {
-        <YOUR CODE HERE>
+    public static int get_word_frequencies(String word, String[] line) {
+        int count = 0;
+        for (int i = 0; i < line.length; i++)
+        {
+            if (word.equals(line[i]))
+                count++;
+        }
+        return count;
     }
-
     /*
         Create a String[] of size numwords that is populated with words a scanner
 
@@ -28,10 +31,13 @@ public class FrequencyList
             String[]: Array of words that is recieved from user via Scanner
 
      */
-    public static String[] get_words(Scanner input, int numwords){
-        <YOUR CODE HERE>
+    public static String[] get_words (Scanner input, int numWords){
+        String[] words = new String[numWords];
+        for (int i = 0; i < words.length; i++) {
+            words[i] = input.next();
+        }
+        return words;
     }
-
     /*
         Go through every element in String[] line and find all
         words
@@ -43,25 +49,30 @@ public class FrequencyList
 
         hint: Arraylists have a method called contains
      */
-    public static ArrayList<String> get_unique_words(String[] line)
+    public static ArrayList<String> get_unique_words (String[] line)
     {
-        <YOUR CODE HERE>
+        ArrayList<String> uniWords = new ArrayList<String>();
+        for(int i = 0; i < line.length; i++) {
+            if (!uniWords.contains(line[i])) {
+                uniWords.add(line[i]);
+            }
+        }
+        return uniWords;
     }
 
-    public static void main(String[] args)
+    public static void main (String[]args)
     {
-        int numwords;
         // Implement a Scanner (call it input)
-        <YOUR CODE HERE>
+        Scanner input = new Scanner(System.in);
         // Get an int from the Scanner and save it to numwords
-        <YOUR CODE HERE>
-
-        String[] line = get_words(input, numwords);
+        //<YOUR CODE HERE>
+        int numWords = input.nextInt();
+        String[] line = get_words(input, numWords);
         ArrayList<String> unique_words = get_unique_words(line);
-        for(int i=0;i< unique_words.size();i++)
-        {
-            int frequency = get_word_frequencies(unique_words.get(i),line);
+        for (int i = 0; i < unique_words.size(); i++) {
+            int frequency = get_word_frequencies(unique_words.get(i), line);
             System.out.println("Word: " + unique_words.get(i) + "\tFrequency: " + frequency);
         }
     }
 }
+
